3754143290cb263f98edcfe43d74b240f6c5a1eb
Eric Fung <Eric.Fung@cerner.com>
2023-07-22 08:02:31 -0500

    Its working

--- src/components/LanguageDropdown.js
+++ src/components/LanguageDropdown.js
@@ -1,16 +1,18 @@
 import React, { useState, useContext } from 'react';
 import 'bootstrap/js/dist/dropdown';
-import { Context } from './wrapper';
+import { LocalContext } from './wrapper';
 
 const LanguageDropdown = ({ title }) => {
-  const [local, setLocal] = useState('en_us');
+  const { locale, selectLang } = useContext(LocalContext);
+
+  const [local, setLocal] = useState(locale);
 
   const handleSelect = (selectedLocal) => {
     setLocal(selectedLocal);
+    selectLang(selectedLocal)
   };
 
   const currentLang = `this page is in ${local}`;
-  const Context = useContext(Context);
 
   return (
     <div className="dropdown">
@@ -27,18 +29,18 @@ const LanguageDropdown = ({ title }) => {
       <ul className="dropdown-menu">
         <li>
           <button
-            className={`dropdown-item ${local === 'en_us' ? 'active' : ''}`}
+            className={`dropdown-item ${local.toLocaleLowerCase() === 'en-us' ? 'active' : ''}`}
             type="button"
-            onClick={() => handleSelect('en_us')}
+            onClick={() => handleSelect('en-us')}
           >
             en-us
           </button>
         </li>
         <li>
           <button
-            className={`dropdown-item ${local === '中文' ? 'active' : ''}`}
+            className={`dropdown-item ${local.toLocaleLowerCase() === 'zh-hk' ? 'active' : ''}`}
             type="button"
-            onClick={() => handleSelect('中文')}
+            onClick={() => handleSelect('zh-hk')}
           >
             中文
           </button>
--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,42 +1,31 @@
-import React, { useState} from "react";
-import { IntlProvider } from "react-intl";
+import React, { useState } from 'react';
+import { IntlProvider } from 'react-intl';
 import messages_en_us from '../translations/en-us.json';
 import messages_zh_hk from '../translations/zh-hk.json';
 
-export const Context = React.createContext();
+export const LocalContext = React.createContext();
 
 const local = navigator.language;
 
 let lang;
-if (local === "en-us") {
+if (local === 'en-us') {
   lang = messages_en_us;
 } else {
   lang = messages_zh_hk;
 }
 
+const Wrapper = ({ children }) => {
+  const [locale, setLocale] = useState(local);
 
-const Wrapper = () => {
-    const [locale, setLocale] = useState(local);
-    const [messages, setMessages] = useState(lang);
+  function selectLang(value) {
+    setLocale(value);
+  }
 
-    function selectLang(e) {
-        const newLocale = e.target.value;
-        setLocale(newLocale);
-        if(newLocale === "es-us") {
-            setMessages(messages_en_us)
-        } else {
-            setMessages(messages_zh_hk);
-        }
+  return (
+    <LocalContext.Provider value={{ locale, selectLang }}>
+        {children}
+    </LocalContext.Provider>
+  );
+};
 
-
-    }
-
-    return(
-        <Context.Provider value= {{ locale, selectLang }}>
-        <IntlProvider messages = {messages} locale={local}>
-        </IntlProvider>
-        </Context.Provider>
-    )
-}
-
-export default Wrapper
+export default Wrapper;
