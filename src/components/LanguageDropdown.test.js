b2512855839d25ec0c88c9dac897e3b37552cfd2
Eric Fung <Eric.Fung@cerner.com>
2023-09-02 07:06:11 -0500

    Fix test

--- src/components/LanguageDropdown.test.js
+++ src/components/LanguageDropdown.test.js
@@ -1,9 +1,26 @@
-import { render } from '@testing-library/react';
-import LanguageDropdown from "./LanguageDropdown";
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import IntlProviderWrapper from './IntlProviderWrapper';
+import LanguageDropdown from './LanguageDropdown';
+import LocalContextProvider from './LocalContextProvider';
 
-test('dropdown should be in the page', () => {
-    const { getByTestId } = render(<LanguageDropdown />)
-    const Dropdown = getByTestId ("dropdown")
-    expect(Dropdown).toBeInTheDocument()
+const mockContextValue = {
+  locale: 'en-us',
+  selectLang: jest.fn(),
+};
 
-  });
+const renderWithProviders = (component) => {
+  return render(
+    <LocalContextProvider value={mockContextValue}>
+      <IntlProviderWrapper>{component}</IntlProviderWrapper>
+    </LocalContextProvider>
+  );
+};
+
+test('renders language dropdown with initial active locale', () => {
+  renderWithProviders(<LanguageDropdown />);
+
+  const dropdownButton = screen.getByTestId('Dropdown');
+  expect(dropdownButton).toHaveTextContent('English');
+});
