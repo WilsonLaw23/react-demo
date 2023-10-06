8e007c3f527f5e5f9f32ce77de7ec4e861d657a4
WilsonLaw23 <fuchungl@yahoo.com.hk>
2023-10-06 17:59:20 +0800

    fix react test

--- src/components/LanguageDropdown.js
+++ src/components/LanguageDropdown.js
@@ -17,6 +17,7 @@ const LanguageDropdown = ({ title }) => {
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
+        data-testid="languageDropdown"
       >
         <FormattedMessage id="localtitle" />
       </button>
